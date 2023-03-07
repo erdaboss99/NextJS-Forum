c399ba1a1ff09d00b32d7841fb816a16763f4bf5
erdaboss99 <60655139+erdaboss99@users.noreply.github.com>
2023-03-07 23:30:10 +0100

    Remove unnecessary files and  install TailwindCSS

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import '@/styles/globals.css';
+import { Shantell_Sans } from '@next/font/google';
+
+const shantellsans = Shantell_Sans({ subsets: ['latin'] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<div className={shantellsans.className}>
+			<Component {...pageProps} />
+		</div>
+	);
 }
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		extend: {
+			colors: {
+				primary: {
+					dark: '#393053',
+					light: '#ECF2FF',
+				},
+				highlight: {
+					dark: '#ECF2FF',
+					light: '#655DBB',
+				},
+				secondary: {
+					dark: '#635985',
+					light: '#BFACE2',
+				},
+				action: '#3E54AC',
+			},
+		},
+	},
+	plugins: [],
+};
