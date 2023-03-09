1d76181fc90bb36a3042126a3bbfe9e67f21a046
erdaboss99 <60655139+erdaboss99@users.noreply.github.com>
2023-03-09 21:02:25 +0100

    Implement navbar minimise feature

--- src/components/common/AdminNav.tsx
+++ src/components/common/AdminNav.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
-import { FC } from 'react';
+import { FC, useRef, useState } from 'react';
 import { IconType } from 'react-icons';
+import { RiMenuFoldFill, RiMenuUnfoldFill } from 'react-icons/ri';
 
 import Logo from './Logo';
 
@@ -8,29 +9,60 @@ interface AdminNavProps {
 	navItems: { href: string; label: string; icon: IconType }[];
 }
 
-const AdminNav: FC<AdminNavProps> = ({ navItems }): JSX.Element => {
-	return (
-		<nav className='h-screen w-48 shadow-sm bg-secondary-light dark:bg-secondary-dark '>
-			<Link className='flex items-center space-x-2 p-3 mb-10' href='/admin'>
-				<Logo className='fill-highlight-light dark:fill-highlight-dark w-5 h-5' />
-				<span className='text-highlight-light dark:text-highlight-dark text-xl font-semibold'>
-					Admin
-				</span>
-			</Link>
+const NAV_EXPANDED_WIDTH = 'w-48';
+const NAV_CLOSED_WIDTH = 'w-12';
 
-			<div className='space-y-6'>
-				{navItems.map((item) => {
-					return (
-						<Link
-							key={item.href}
-							className='flex items-center text-highlight-light dark:text-highlight-dark text-xl p-3 hover:scale-[0.98] transition'
-							href={item.href}>
-							<item.icon size={24} />
-							<span className='ml-2'>{item.label}</span>
-						</Link>
-					);
-				})}
+const AdminNav: FC<AdminNavProps> = ({ navItems }): JSX.Element => {
+	const navRef = useRef<HTMLElement>(null);
+	const [expandedNav, setExpandedNav] = useState(true);
+	const updateNavState = () => {
+		const currentNav = navRef.current;
+		if (!currentNav) return;
+
+		const { classList } = currentNav;
+		if (expandedNav) {
+			classList.replace(NAV_EXPANDED_WIDTH, NAV_CLOSED_WIDTH);
+		} else {
+			classList.replace(NAV_CLOSED_WIDTH, NAV_EXPANDED_WIDTH);
+		}
+		setExpandedNav(!expandedNav);
+	};
+
+	return (
+		<nav
+			ref={navRef}
+			className='h-screen w-48 shadow-sm bg-secondary-light dark:bg-secondary-dark flex flex-col justify-between'>
+			<div>
+				<Link className='flex items-center space-x-2 p-3 mb-10' href='/admin'>
+					<Logo className='fill-highlight-light dark:fill-highlight-dark w-5 h-5' />
+
+					{expandedNav && (
+						<span className='text-highlight-light dark:text-highlight-dark text-xl font-semibold'>
+							Admin
+						</span>
+					)}
+				</Link>
+
+				<div className='space-y-6'>
+					{navItems.map((item) => {
+						return (
+							<Link
+								key={item.href}
+								className='flex items-center text-highlight-light dark:text-highlight-dark text-xl p-3 hover:scale-[0.98] transition'
+								href={item.href}>
+								<item.icon size={24} />
+								{expandedNav && <span className='ml-2'>{item.label}</span>}
+							</Link>
+						);
+					})}
+				</div>
 			</div>
+
+			<button
+				onClick={updateNavState}
+				className='text-highlight-light dark:text-highlight-dark p-3 hover:scale-[0.98] transition self-end'>
+				{expandedNav ? <RiMenuFoldFill size={25} /> : <RiMenuUnfoldFill size={25} />}
+			</button>
 		</nav>
 	);
 };
