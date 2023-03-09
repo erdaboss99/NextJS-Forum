8d620a58aa764b61019dd627cc3ec7e8ea03fac7
erdaboss99 <60655139+erdaboss99@users.noreply.github.com>
2023-03-09 20:19:25 +0100

    Add items to the admin dashboard

--- src/components/common/AdminNav.tsx
+++ src/components/common/AdminNav.tsx
@@ -1,18 +1,36 @@
 import Link from 'next/link';
 import { FC } from 'react';
+import { IconType } from 'react-icons';
+
 import Logo from './Logo';
 
-interface AdminNavProps {}
+interface AdminNavProps {
+	navItems: { href: string; label: string; icon: IconType }[];
+}
 
-const AdminNav: FC<AdminNavProps> = (props): JSX.Element => {
+const AdminNav: FC<AdminNavProps> = ({ navItems }): JSX.Element => {
 	return (
-		<nav className='h-screen w-60 shadow-sm bg-secondary-light dark:bg-secondary-dark '>
-			<Link className='flex items-center space-x-2 p-3' href='/admin'>
+		<nav className='h-screen w-48 shadow-sm bg-secondary-light dark:bg-secondary-dark '>
+			<Link className='flex items-center space-x-2 p-3 mb-10' href='/admin'>
 				<Logo className='fill-highlight-light dark:fill-highlight-dark w-5 h-5' />
 				<span className='text-highlight-light dark:text-highlight-dark text-xl font-semibold'>
 					Admin
 				</span>
 			</Link>
+
+			<div className='space-y-6'>
+				{navItems.map((item) => {
+					return (
+						<Link
+							key={item.href}
+							className='flex items-center text-highlight-light dark:text-highlight-dark text-xl p-3 hover:scale-[0.98] transition'
+							href={item.href}>
+							<item.icon size={24} />
+							<span className='ml-2'>{item.label}</span>
+						</Link>
+					);
+				})}
+			</div>
 		</nav>
 	);
 };
--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,12 +1,25 @@
 import AdminNav from '@/components/common/AdminNav';
 import { NextPage } from 'next';
+import {
+	AiOutlineContainer,
+	AiOutlineDashboard,
+	AiOutlineMail,
+	AiOutlineTeam,
+} from 'react-icons/ai';
 
 interface AdminPageProps {}
+
+const navItems = [
+	{ href: '/admin', icon: AiOutlineDashboard, label: 'Dashboard' },
+	{ href: '/admin/posts', icon: AiOutlineContainer, label: 'Posts' },
+	{ href: '/admin/users', icon: AiOutlineTeam, label: 'Users' },
+	{ href: '/admin/comments', icon: AiOutlineMail, label: 'Comments' },
+];
 
 const AdminPage: NextPage<AdminPageProps> = () => {
 	return (
 		<div className=''>
-			<AdminNav />
+			<AdminNav navItems={navItems} />
 		</div>
 	);
 };
