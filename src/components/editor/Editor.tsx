9077e08a5f4b017dbfacbc124e3b4c8cbf926bb7
erdaboss99 <60655139+erdaboss99@users.noreply.github.com>
2023-03-10 14:57:02 +0100

    Implement button component for the basic styles

--- src/components/editor/Editor.tsx
+++ src/components/editor/Editor.tsx
@@ -1,14 +1,15 @@
 import { FC } from 'react';
 import { EditorContent, useEditor } from '@tiptap/react';
 import StarterKit from '@tiptap/starter-kit';
+import Underline from '@tiptap/extension-underline';
 import Toolbar from './toolbar/Toolbar';
 
 interface EditorProps {}
 
 const Editor: FC<EditorProps> = (props): JSX.Element => {
-	const editor = useEditor({ extensions: [StarterKit] });
+	const editor = useEditor({ extensions: [StarterKit, Underline] });
 	return (
-		<div>
+		<div className='p-3 bg-primary-light dark:bg-primary-dark'>
 			<Toolbar editor={editor} />
 			<EditorContent editor={editor} />
 		</div>
--- src/components/editor/toolbar/Toolbar.tsx
+++ src/components/editor/toolbar/Toolbar.tsx
@@ -1,9 +1,24 @@
+import React from 'react';
 import { FC } from 'react';
 import { Editor } from '@tiptap/react';
 import DropdownOptions from '@/components/common/DropdownOptions';
+import Button from './Button';
 import { getFocusedEditor } from '../editorUtils';
 import { AiFillCaretDown } from 'react-icons/ai';
-
+import {
+	BsTypeBold,
+	BsTypeItalic,
+	BsTypeUnderline,
+	BsTypeStrikethrough,
+	BsCode,
+	BsBraces,
+	BsLink45Deg,
+	BsListOl,
+	BsListUl,
+	BsImageFill,
+	BsYoutube,
+} from 'react-icons/bs';
+import { RiDoubleQuotesL } from 'react-icons/ri';
 interface ToolbarProps {
 	editor: Editor | null;
 }
@@ -46,9 +61,51 @@ const Toolbar: FC<ToolbarProps> = ({ editor }): JSX.Element | null => {
 		);
 	};
 
+	const buttons = [
+		{
+			icon: BsTypeBold,
+			separatorBefore: true,
+			onClick: () => getFocusedEditor(editor).toggleBold().run(),
+		},
+		{ icon: BsTypeItalic, onClick: () => getFocusedEditor(editor).toggleItalic().run() },
+		{ icon: BsTypeUnderline, onClick: () => getFocusedEditor(editor).toggleUnderline().run() },
+		{ icon: BsTypeStrikethrough, onClick: () => getFocusedEditor(editor).toggleStrike().run() },
+		{
+			icon: RiDoubleQuotesL,
+			separatorBefore: true,
+			onClick: () => getFocusedEditor(editor).toggleBlockquote().run(),
+		},
+		{ icon: BsCode, onClick: () => getFocusedEditor(editor).toggleCode().run() },
+		{ icon: BsBraces, onClick: () => getFocusedEditor(editor).toggleCodeBlock().run() },
+		{ icon: BsLink45Deg, onClick: () => {} }, //TODO
+		{ icon: BsListOl, onClick: () => getFocusedEditor(editor).toggleOrderedList().run() },
+		{ icon: BsListUl, onClick: () => getFocusedEditor(editor).toggleBulletList().run() },
+		{ icon: BsYoutube, separatorBefore: true, onClick: () => {} }, //TODO
+		{ icon: BsImageFill, onClick: () => {} }, //TODO
+	];
+
 	return (
-		<div>
+		<div className='flex items-center'>
 			<DropdownOptions options={options} head={<Head />} />
+
+			<div className='flex items-center space-x-3'>
+				{buttons.map((item, index) => {
+					return (
+						<React.Fragment key={'f' + index}>
+							{item.separatorBefore && (
+								<div
+									key={'s' + index}
+									className='h-4 w-[1px] bg-secondary-dark dark:bg-secondary-light mx-8'
+								/>
+							)}
+
+							<Button key={'b' + index} onClick={item.onClick}>
+								<item.icon />
+							</Button>
+						</React.Fragment>
+					);
+				})}
+			</div>
 		</div>
 	);
 };
