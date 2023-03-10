022e17629cf9e66327ba5971947769c1b22495a7
erdaboss99 <60655139+erdaboss99@users.noreply.github.com>
2023-03-10 14:54:05 +0100

    Create button component for the styles

---
+++ src/components/editor/toolbar/Button.tsx
@@ -0,0 +1,38 @@
+import { FC, MouseEventHandler, ReactNode, useCallback } from 'react';
+
+interface ButtonProps {
+	children: ReactNode;
+	active?: boolean;
+	disabled?: boolean;
+	onMouseDown?: MouseEventHandler<HTMLButtonElement>;
+	onClick?: MouseEventHandler<HTMLButtonElement>;
+}
+
+const Button: FC<ButtonProps> = ({
+	children,
+	active,
+	disabled,
+	onMouseDown,
+	onClick,
+}): JSX.Element => {
+	const getActiveStyle = useCallback((): string => {
+		if (active)
+			return 'bg-primary-dark dark:bg-primary-light text-primary-light dark:text-primary-dark ';
+		else return 'bg-secondary-dark text-secondary-light';
+	}, [active]);
+
+	const commonClasses = 'p-2 rounded text-lg hover:scale-110 hover:shadow:md transition ';
+
+	return (
+		<button
+			type='button'
+			onMouseDown={onMouseDown}
+			onClick={onClick}
+			className={commonClasses + getActiveStyle()}
+			disabled={disabled}>
+			{children}
+		</button>
+	);
+};
+
+export default Button;
