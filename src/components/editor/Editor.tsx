633d01736fc096b2a14fea608100f3d5cbef1463
erdaboss99 <60655139+erdaboss99@users.noreply.github.com>
2023-03-10 15:10:26 +0100

    Fix formatting and add placeholder for the editor

--- src/components/editor/Editor.tsx
+++ src/components/editor/Editor.tsx
@@ -2,15 +2,28 @@ import { FC } from 'react';
 import { EditorContent, useEditor } from '@tiptap/react';
 import StarterKit from '@tiptap/starter-kit';
 import Underline from '@tiptap/extension-underline';
+import Placeholder from '@tiptap/extension-placeholder';
 import Toolbar from './toolbar/Toolbar';
 
 interface EditorProps {}
 
 const Editor: FC<EditorProps> = (props): JSX.Element => {
-	const editor = useEditor({ extensions: [StarterKit, Underline] });
+	const editor = useEditor({
+		extensions: [
+			StarterKit,
+			Underline,
+			Placeholder.configure({ placeholder: 'Type something...' }),
+		],
+		editorProps: {
+			attributes: {
+				class: 'prose prose-lg focus:outline-none dark:prose-invert max-w-full mx-auto h-full',
+			},
+		},
+	});
 	return (
 		<div className='p-3 bg-primary-light dark:bg-primary-dark'>
 			<Toolbar editor={editor} />
+			<div className='h-[1px] w-full bg-secondary-dark dark:bg-secondary-light my-3' />
 			<EditorContent editor={editor} />
 		</div>
 	);
