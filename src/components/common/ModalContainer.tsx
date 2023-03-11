a05f8985cd632799fd0f2ef98563758246746659
erdaboss99 <60655139+erdaboss99@users.noreply.github.com>
2023-03-11 13:11:54 +0100

    Improve closing for the gallery modal

--- src/components/common/ModalContainer.tsx
+++ src/components/common/ModalContainer.tsx
@@ -1,4 +1,4 @@
-import { FC, ReactNode } from 'react';
+import { FC, MouseEventHandler, ReactNode, useCallback, useEffect, useId } from 'react';
 
 interface ModalContainerProps {
 	visible?: boolean;
@@ -11,14 +11,25 @@ const ModalContainer: FC<ModalContainerProps> = ({
 	children,
 	onClose,
 }): JSX.Element | null => {
-	if (!visible) return null;
+	const containerId = useId();
+	const handleClose = useCallback(() => onClose && onClose(), [onClose]);
 
-	const handleClick = () => {
-		onClose && onClose();
+	const handleClick: MouseEventHandler<HTMLDivElement> = (e) => {
+		if ((e.target as HTMLDivElement).id === containerId) handleClose();
 	};
+
+	useEffect(() => {
+		const closeModal = (e: KeyboardEvent) => e.key === 'Escape' && handleClose();
+		document.addEventListener('keydown', closeModal);
+
+		return () => document.removeEventListener('keydown', closeModal);
+	}, [handleClose]);
+
+	if (!visible) return null;
 
 	return (
 		<div
+			id={containerId}
 			onClick={handleClick}
 			className='fixed inset-0 bg-primary-light dark:bg-primary-dark bg-opacity-5 dark:bg-opacity-5 backdrop-blur-[2px] z-50 flex items-center justify-center'>
 			{children}
