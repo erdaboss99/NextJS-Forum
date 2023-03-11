213d8bcca8fdb1df546526edfd182c83e1c77b47
erdaboss99 <60655139+erdaboss99@users.noreply.github.com>
2023-03-11 22:06:52 +0100

    Implement basic image insertion functionality

--- src/components/editor/gallery/Modal.tsx
+++ src/components/editor/gallery/Modal.tsx
@@ -1,5 +1,6 @@
 import { FC, ReactNode } from 'react';
 import ModalContainer from '@/components/common/ModalContainer';
+import Gallery from './Gallery';
 
 interface ModalProps {
 	children: ReactNode;
@@ -10,8 +11,14 @@ interface ModalProps {
 const Modal: FC<ModalProps> = ({ visible, children, onClose }): JSX.Element => {
 	return (
 		<ModalContainer visible={visible} onClose={onClose}>
-			<div className='bg-black p-20'>
-				<button className='bg-white p-3'>Click me</button>
+			<div className='max-w-4xl p-2 bg-primary-dark dark:bg-primary-light rounded'>
+				<div className='flex'>
+					<div className='basis-[75%] max-h-[450px] overflow-y-auto custom-scroll-bar'>
+						<Gallery />
+					</div>
+
+					<div className='basis-[25%]'></div>
+				</div>
 			</div>
 		</ModalContainer>
 	);
